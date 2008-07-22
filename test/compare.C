16e9369a25ab308196f71c6d04238d64ef97e1c6
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2008-07-22 10:19:44 +0000

    First commit

---
+++ test/compare.C
@@ -0,0 +1,174 @@
+
+#include<vector>
+#include<map>
+#include<iterator>
+
+#include "TROOT.h"
+#include "TFile.h"
+#include "TSystem.h"
+#include "TH1.h"
+
+
+//// Set input files and options!! ////
+
+TString titleOld = "CMSSW_2_1_0_pre6, 4.0 T";
+TString titleNew = "CMSSW_2_1_0_pre6, 3.8 T";
+
+TString sampleType = " (Zmumu)";
+TString fileType   = ".pdf";
+bool removeHlt = false;
+
+TString pathRateEfficienciesOld = "";
+TString pathRateEfficienciesNew = "";
+TString pathDistributionsOld = "";
+TString pathDistributionsNew = "";
+
+///////////////////////////////////////
+
+void compare()
+{
+
+  TFile *Old = TFile::Open("ZMM_40.root","READ");
+  TFile *New = TFile::Open("ZMM_38.root","READ");
+
+  gStyle->SetOptStat(0);
+  gStyle->SetErrorX(0.5);
+
+  // Create a new canvas.
+  c1 = new TCanvas("c1","c1",800,600);
+  c1->GetFrame()->SetBorderSize(6);
+  c1->GetFrame()->SetBorderMode(-1);
+
+  Old->cd()  ;
+  gDirectory->ReadKeys();
+  TDirectory *dirOld = gDirectory;
+  New->cd()  ;
+  gDirectory->ReadKeys();
+  TDirectory *dirNew = gDirectory;
+    
+  TH1F *hOld, *hNew;
+
+  int counter = 0;
+  vector<TString> histNames;
+  TString path;
+
+  histNames.clear();
+  path = "/Rates/RateEfficiencies/hltSingleMuIso/";
+  histNames.push_back("MCTurnOn_hltSingleMuIsoL1Filtered"); 
+  for ( int i = 0; i < histNames.size(); i++ ) {
+    TString pathOld = pathRateEfficienciesOld + histNames.at(i);
+    if ( removeHlt ) pathOld.ReplaceAll("hlt","");
+    TString pathNew = pathRateEfficienciesNew + histNames.at(i);
+    hOld = (TH1F *)dirOld->Get(pathOld); 
+    hNew = (TH1F *)dirNew->Get(pathNew); 
+    plot( hOld, hNew, ++counter );
+  }
+  
+  histNames.clear();
+  path = "/Rates/Distributions/hltSingleMuIso/";
+  histNames.push_back("MCTurnOn_hltSingleMuIsoL2PreFiltered"); 
+  histNames.push_back("MCTurnOn_hltSingleMuIsoL2IsoFiltered"); 
+  histNames.push_back("MCTurnOn_hltSingleMuIsoL3PreFiltered"); 
+  histNames.push_back("MCTurnOn_hltSingleMuIsoL3IsoFiltered"); 
+  histNames.push_back("MCeta_hltSingleMuIsoL1Filtered"); 
+  histNames.push_back("MCeta_hltSingleMuIsoL2PreFiltered"); 
+  histNames.push_back("MCeta_hltSingleMuIsoL2IsoFiltered"); 
+  histNames.push_back("MCeta_hltSingleMuIsoL3PreFiltered"); 
+  histNames.push_back("MCeta_hltSingleMuIsoL3IsoFiltered"); 
+  histNames.push_back("MCphi_hltSingleMuIsoL1Filtered"); 
+  histNames.push_back("MCphi_hltSingleMuIsoL2PreFiltered"); 
+  histNames.push_back("MCphi_hltSingleMuIsoL2IsoFiltered"); 
+  histNames.push_back("MCphi_hltSingleMuIsoL3PreFiltered"); 
+  histNames.push_back("MCphi_hltSingleMuIsoL3IsoFiltered"); 
+  for ( int i = 0; i < histNames.size(); i++ ) {
+    TString pathOld = pathDistributionsOld + histNames.at(i);
+    if ( removeHlt ) pathOld.ReplaceAll("hlt","");
+    TString pathNew = pathDistributionsOld + histNames.at(i);
+    hOld = (TH1F *)dirOld->Get(pathOld); 
+    hNew = (TH1F *)dirNew->Get(pathNew); 
+    plot( hOld, hNew, ++counter );
+  }
+
+  histNames.clear();
+  path = "/Rates/RateEfficiencies/hltSingleMuIso/";
+  histNames.push_back("RECOTurnOn_hltSingleMuIsoL1Filtered"); 
+  for ( int i = 0; i < histNames.size(); i++ ) {
+    TString pathOld = pathRateEfficienciesOld + histNames.at(i);
+    if ( removeHlt ) pathOld.ReplaceAll("hlt","");
+    TString pathNew = pathRateEfficienciesNew + histNames.at(i);
+    hOld = (TH1F *)dirOld->Get(pathOld); 
+    hNew = (TH1F *)dirNew->Get(pathNew); 
+    plot( hOld, hNew, ++counter );
+  }
+
+  histNames.clear();
+  path = "/Rates/Distributions/hltSingleMuIso/";
+  histNames.push_back("RECOTurnOn_hltSingleMuIsoL2PreFiltered"); 
+  histNames.push_back("RECOTurnOn_hltSingleMuIsoL2IsoFiltered"); 
+  histNames.push_back("RECOTurnOn_hltSingleMuIsoL3PreFiltered"); 
+  histNames.push_back("RECOTurnOn_hltSingleMuIsoL3IsoFiltered"); 
+  histNames.push_back("RECOeta_hltSingleMuIsoL1Filtered"); 
+  histNames.push_back("RECOeta_hltSingleMuIsoL2PreFiltered"); 
+  histNames.push_back("RECOeta_hltSingleMuIsoL2IsoFiltered"); 
+  histNames.push_back("RECOeta_hltSingleMuIsoL3PreFiltered"); 
+  histNames.push_back("RECOeta_hltSingleMuIsoL3IsoFiltered"); 
+  histNames.push_back("RECOphi_hltSingleMuIsoL1Filtered"); 
+  histNames.push_back("RECOphi_hltSingleMuIsoL2PreFiltered"); 
+  histNames.push_back("RECOphi_hltSingleMuIsoL2IsoFiltered"); 
+  histNames.push_back("RECOphi_hltSingleMuIsoL3PreFiltered"); 
+  histNames.push_back("RECOphi_hltSingleMuIsoL3IsoFiltered"); 
+  for ( int i = 0; i < histNames.size(); i++ ) {
+    TString pathOld = pathDistributionsOld + histNames.at(i);
+    if ( removeHlt ) pathOld.ReplaceAll("hlt","");
+    TString pathNew = pathDistributionsOld + histNames.at(i);
+    hOld = (TH1F *)dirOld->Get(pathOld); 
+    hNew = (TH1F *)dirNew->Get(pathNew); 
+    plot( hOld, hNew, ++counter );
+  }
+}
+
+
+
+void plot( TH1* hOld, TH1* hNew, int counter ) {
+
+  TString genReco, yTitle;
+
+  TString name = hOld->GetName();
+  TString histTitle = hOld->GetTitle();
+  TString label = histTitle("label.*");
+  label.ReplaceAll("label=","");
+  TString title = histTitle(".*label");
+  title.Remove( title.First('label'), title.Length() );
+  if ( name.Contains("RECO") ) genReco = " (Reconstructed)";
+  else if ( name.Contains("MC") ) genReco = " (Generated)";
+  if ( name.Contains("L1") ) yTitle = "L1 pass / Gen (%)";
+  else if ( name.Contains("L2") ) yTitle = "L2 pass / L1 pass (%)";
+  else if ( name.Contains("L3") ) yTitle = "L3 pass / L1 pass (%)";
+
+  hOld->SetTitle(titleOld);
+  hNew->SetTitle(titleNew);
+  hOld->SetLineWidth(2);
+  hNew->SetLineWidth(2);
+  hOld->SetLineColor(2);
+  hNew->SetLineColor(4);
+  hOld->GetYaxis()->SetRangeUser(0.,105.);
+  hOld->GetYaxis()->SetTitle(yTitle);
+
+  // Fix bug in analyzer that incorrectly sets some x-axis titles
+  TString xTitle = hOld->GetXaxis()->GetTitle(); 
+  if ( xTitle.Contains("Muon #") && 
+       !xTitle.Contains("Gen") && !xTitle.Contains("Reco") )
+    if (genReco.Contains("Gen")) hOld->GetXaxis()->SetTitle("Gen "+xTitle);
+    else hOld->GetXaxis()->SetTitle("Reco "+xTitle);
+
+  hOld->Draw();
+  hNew->Draw("same");
+  gPad->BuildLegend(0.35,0.15,0.85,0.3,label+genReco);
+  TString number = Form("%.2i",counter);
+  hOld->SetTitle(number+": "+label+sampleType);
+  gPad->Update();
+  c1->SaveAs(number+"_"+name+fileType);
+
+}
+
+
