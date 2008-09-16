31359a326feb8f8138c3ebf999d3998d491add62
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2008-09-16 18:43:46 +0000

    Initial move to python configuration

---
+++ test/PostProcessor_cfg.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("EDMtoMEConvert")
+
+process.load("DQMServices.Components.EDMtoMEConverter_cff")
+process.load("DQMServices.Components.DQMEnvironment_cfi")
+process.load("HLTriggerOffline.Muon.PostProcessor_cfi")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+process.MessageLogger = cms.Service("MessageLogger")
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:RateTimeAnalyzer.root')
+)
+
+process.path1 = cms.Path(process.EDMtoMEConverter*process.postProcessor)
+process.DQMStore.referenceFileName = ''
---
+++ test/muonTriggerRateTimeAnalyzer_cfg.py
@@ -0,0 +1,62 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("HLTMuonOfflineAnalysis")
+
+process.load("HLTriggerOffline.Muon.muonTriggerRateTimeAnalyzer_cfi")
+process.load("DQMServices.Components.MEtoEDMConverter_cfi")
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+process.source = cms.Source("PoolSource",
+    skipEvents = cms.untracked.uint32(0),
+    fileNames  = cms.untracked.vstring()
+)
+
+# By default, a fileList_cff is required; appropriate examples can be found
+# in /afs/cern.ch/user/k/klukas/webHLT/fileLists/ and copied to ../python
+import HLTriggerOffline.Muon.fileList_cff as fileList
+process.source.fileNames = fileList.fileNames
+
+process.DQMStore = cms.Service("DQMStore")
+
+process.filter = cms.EDFilter("MCSingleParticleFilter",
+    MaxEta     = cms.untracked.vdouble(  2.1,  2.1 ),
+    MinEta     = cms.untracked.vdouble( -2.1, -2.1 ),
+    MinPt      = cms.untracked.vdouble(  5.0,  5.0 ),
+    ParticleID = cms.untracked.vint32 (  -13,   13 )
+)
+
+process.MessageLogger = cms.Service("MessageLogger",
+    HLTMuonVallog  = cms.untracked.PSet(
+        threshold  = cms.untracked.string('INFO'),
+        default    = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
+        HLTMuonVal = cms.untracked.PSet(
+            limit = cms.untracked.int32(100000)
+        )
+    ),
+    debugModules   = cms.untracked.vstring('RateAnalyzer'),
+    cout           = cms.untracked.PSet(
+            threshold = cms.untracked.string('WARNING')
+    ),
+    categories     = cms.untracked.vstring('HLTMuonVal'),
+    destinations   = cms.untracked.vstring('cout', 'HLTMuonVal.log')
+)
+
+process.out = cms.OutputModule("PoolOutputModule",
+    outputCommands = cms.untracked.vstring(
+        'drop *', 
+        'keep *_MEtoEDMConverter_*_*'),
+    fileName = cms.untracked.string('RateTimeAnalyzer.root')
+)
+
+process.analyzerpath = cms.Path(
+    process.filter*
+    process.muonTriggerRateTimeAnalyzer*
+    process.MEtoEDMConverter
+)
+
+process.outpath = cms.EndPath(process.out)
