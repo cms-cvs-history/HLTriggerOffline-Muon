6c8c5b3ec3d9fee3457df9ed7c9b0e0fd2abbac3
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2008-09-18 01:36:16 +0000

    Added templates for changing MinPt and MotherParticleId

--- test/muonTriggerRateTimeAnalyzer_cfg.py
+++ test/muonTriggerRateTimeAnalyzer_cfg.py
@@ -4,6 +4,8 @@ process = cms.Process("HLTMuonOfflineAnalysis")
 
 process.load("HLTriggerOffline.Muon.muonTriggerRateTimeAnalyzer_cfi")
 process.load("DQMServices.Components.MEtoEDMConverter_cfi")
+# muonTriggerRateTimeAnalyzer.MinPt = cms.untracked.double(5.)
+# muonTriggerRateTimeAnalyzer.MotherParticle = cms.untracked.uint32(24)
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
