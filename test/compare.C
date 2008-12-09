7feffae3a7bca891c4db101b7ba18075bdb05018
Jeff Klukas <sha1-74553239da996b6efadda9b9e522888ba73021ad@cern.ch>
2008-12-09 21:48:13 +0000

    Added TPRegexp to parse int & float MonitorElements

--- test/compare.C
+++ test/compare.C
@@ -1,19 +1,20 @@
 
 #include<vector>
+#include<map>
 
 #include "TFile.h"
 #include "TH1.h"
 #include "TList.h"
 
-//// Set input files and options!! ////
+//// Set input files and options ////
 
 TString fileType = ".pdf";
 TString pathDistributions = "/DQMData/HLT/Muon/Distributions/";
-double  cutValPt  = 10.0;
-double  cutValEta =  2.1;
-TString cutEta    = Form(" (|#eta| < %.1f)", cutValEta);
-TString cutPtEta  = Form(" (p_{T} > %.0f GeV, |#eta| < %.1f)", 
-			 cutValPt, cutValEta);
+
+//// Global variables            ////
+
+TString cutEta;
+TString cutPtEta;
 
 ///////////////////////////////////////
 
@@ -22,6 +23,14 @@ void compare( TString n1 = "", TString n2 = "", TString n3 = "",
 	      TString n7 = "", TString n8 = "", 
 	      bool statsOnly = false, TString hltPath = "HLT_IsoMu9" )
 {
+
+  gStyle->SetOptStat(  0);
+  gStyle->SetOptFit (  0);
+  gStyle->SetErrorX (0.5);
+
+  bool isSingleMuSample = ( n1.Contains("SingleMu") ) ? true : false;
+
+  //// Parse filenames, titles, etc.
 
   vector<TString> names;
   if ( n1 != "" ) names.push_back(n1);
@@ -42,39 +51,56 @@ void compare( TString n1 = "", TString n2 = "", TString n3 = "",
 
   const int numFiles = names.size();
   TFile *files[numFiles];
-  TDirectory *dirs[numFiles];
   for ( int i = 0; i < numFiles; i++ ) {
     files[i] = TFile::Open(names[i],"READ"); 
     files[i]->cd();
-    gDirectory->ReadKeys();
-    dirs[i] = gDirectory;
   }
   
   vector<TString> pathNames;
-  dirs[0]->cd(pathDistributions);
+  files[0]->cd(pathDistributions);
+  //files[0]->cd(pathDistributions + hltPath);
   TList *list  = gDirectory->GetListOfKeys();
   TObject *obj = list->First();
   while ( obj ) {
     TString name = obj->GetName();
+    //cout << name << endl;
     pathNames.push_back(name);
     obj = list->After(obj);
   } 
 
-  gStyle->SetOptStat(  0);
-  gStyle->SetOptFit (  0);
-  gStyle->SetErrorX (0.5);
-
+  //// Create map of values of float MonitorElements
+  
+  map<string,float> meValues;
+  TPRegexp regexp("^<(.*)>(i|f|s|qr)=(.*)</\\1>$");
+  files[0]->cd(pathDistributions + hltPath);
+  TList *list = gDirectory->GetListOfKeys();
+  TObject *obj = list->First();
+  while ( obj ) {
+    if ( regexp.Match(obj->GetName()) ) {
+      TObjArray* array = regexp.MatchS(obj->GetName());
+      string meName  = (array->At(1))->GetName();
+      float  meValue = atof( (array->At(3))->GetName() );
+      meValues[meName] = meValue;
+    }
+    obj = list->After(obj);
+  }
+  TString cutEta    = Form(" (|#eta| < %.1f)", meValues["MaxEtaCut"]);
+  TString cutPtEta  = Form(" (p_{T} > %.0f, |#eta| < %.1f)", 
+			   meValues["MinPtCut"], meValues["MaxEtaCut"]);
+  
   c1 = new TCanvas("c1","c1",800,600);
   c1->GetFrame()->SetBorderSize(6);
   c1->GetFrame()->SetBorderMode(-1);
+
+  //// Get names of histograms to look for
 
   int counter = 0;		   
   vector<TString> histNames;
   vector<TString> histNamesStats;
   vector<TString> filterNames;
-  filterNames.push_back("");
+  filterNames.push_back(""); // Holder for L1Filter name
 
-  dirs[0]->cd(pathDistributions + hltPath);
+  files[0]->cd(pathDistributions + hltPath);
   TList *list  = gDirectory->GetListOfKeys();
   TObject *obj = list->First();
   while ( obj ) {
@@ -88,8 +114,10 @@ void compare( TString n1 = "", TString n2 = "", TString n3 = "",
     }
     obj = list->After(obj);
   }
+  if ( filterNames.size() == 5 ) filterNames = sortFilterNames(filterNames);
 
-  for ( int i = 0; i < filterNames.size(); i++ ) histNames.push_back("genTurnOn_"+filterNames[i]);
+  if ( isSingleMuSample ) for ( int i = 0; i < filterNames.size(); i++ ) histNames.push_back("genPassPt_"+filterNames[i]);
+  else for ( int i = 0; i < filterNames.size(); i++ ) histNames.push_back("genTurnOn_"+filterNames[i]);
   for ( int i = 0; i < filterNames.size(); i++ ) histNames.push_back("genEffEta_"+filterNames[i]);
   for ( int i = 0; i < filterNames.size(); i++ ) histNames.push_back("genEffPhi_"+filterNames[i]);
 
@@ -106,16 +134,16 @@ void compare( TString n1 = "", TString n2 = "", TString n3 = "",
   vector<TString> outTwiki(numFiles);
   int stepNumber;
   
-  //// Generate statistics table and make a variety of histograms for histPath
+  //// Generate statistics table and make various histograms for histPath
 
-  for ( int genRecIndex = 0; genRecIndex < 2; genRecIndex++ ) {
+  for ( int genRecIndex = 0; genRecIndex < 1; genRecIndex++ ) {
     for ( int iFile = 0; iFile < numFiles; iFile++ ) {
       histEntries [iFile].clear();
       efficiencies[iFile].clear();
       for ( int i = 0; i < histNamesStats.size(); i++ ) {
 	if ( genRecIndex == 1 ) histNamesStats[i].ReplaceAll("gen","rec");
 	TString histPath = pathDistributions + hltPath +"/"+ histNamesStats[i];
-	TH1F* hist = (TH1F*)dirs[iFile]->Get(histPath);
+	TH1F* hist = (TH1F*)files[iFile]->Get(histPath);
 	if ( hist ) histEntries[iFile].push_back(hist->GetEntries());
       }
       if ( histEntries[iFile].size() > 1 )  
@@ -140,7 +168,7 @@ void compare( TString n1 = "", TString n2 = "", TString n3 = "",
       if ( genRecIndex == 1 ) histNames[i].ReplaceAll("gen","rec");
       TString histPath = pathDistributions + hltPath + "/" + histNames[i];
       for ( int iFile = 0; iFile < numFiles; iFile++ ) {
-	TH1F* hist = (TH1F*)dirs[iFile]->Get(histPath); 
+	TH1F* hist = (TH1F*)files[iFile]->Get(histPath); 
 	TH1F* histClone = (TH1F*)hist->Clone();
 	histClone->SetTitle(titles[iFile]);
 	hists->Add(histClone);
@@ -159,9 +187,10 @@ void compare( TString n1 = "", TString n2 = "", TString n3 = "",
   //// Generate turn-on curves for all paths
 
   for ( int iPath = 0; iPath < pathNames.size(); iPath++ ) {
+    if ( isSingleMuSample ) break;
     filterNames.clear();
     filterNames.push_back("");
-    dirs[0]->cd(pathDistributions + pathNames[iPath]);
+    files[0]->cd(pathDistributions + pathNames[iPath]);
     list = gDirectory->GetListOfKeys();
     obj  = list->First();
     while ( obj ) {
@@ -176,21 +205,13 @@ void compare( TString n1 = "", TString n2 = "", TString n3 = "",
       obj = list->After(obj);
     }
 
-    if ( filterNames.size() == 5 ) {
-      TString temp;
-      temp = filterNames[1];
-      filterNames[1] = filterNames[2];
-      filterNames[2] = temp;
-      temp = filterNames[3];
-      filterNames[3] = filterNames[4];
-      filterNames[4] = temp;
-    }
+    if ( filterNames.size() == 5 ) filterNames = sortFilterNames(filterNames);
 
     for ( int i = 0; i < filterNames.size(); i++ ) {
       TList *hists = new TList();
       for ( int iFile = 0; iFile < numFiles; iFile++ ) {
 	TString histPath = pathDistributions + pathNames[iPath] + "/genTurnOn_" + filterNames[i];
-	TH1F* hist = (TH1F*)dirs[iFile]->Get(histPath); 
+	TH1F* hist = (TH1F*)files[iFile]->Get(histPath); 
 	if ( hist ) {
 	  TH1F* histClone = (TH1F*)hist->Clone();
 	  histClone->SetTitle(titles[iFile]);
@@ -214,16 +235,35 @@ void compare( TString n1 = "", TString n2 = "", TString n3 = "",
       gSystem->Exec("cp merged.pdf "+titles[0]+"_vs_Many.pdf");
     gSystem->Exec("rm [0-9]*.pdf");
   }
+  /**/
+}
+
+
+vector<TString> sortFilterNames( vector<TString> filterNames ) 
+{
+  TString temp;
+  temp = filterNames[1];
+  filterNames[1] = filterNames[2];
+  filterNames[2] = temp;
+  temp = filterNames[3];
+  filterNames[3] = filterNames[4];
+  filterNames[4] = temp;
+  return filterNames;
 }
 
 
 
-void plot( TList *hists, TString histName, vector<double> &effs, int counter ) {
+void plot( TList *hists, TString histName, vector<double> &effs, int counter ) 
+{
 
   TH1F *firstHist = hists->First();
   TH1F *lastHist  = hists->Last();
   TH1F* hist; 
   TString label = histName("L[1-3].*");
+
+  TString histType = "Efficiency";
+  if ( histName.Contains("TurnOn") ) histType = "TurnOn"; 
+  if ( histName.Contains("PassPt") ) histType = "Pt Spectrum";
 
   const int colors[] = { kBlue,     kRed,    kGreen+2, kMagenta+2, 
 			 kYellow+2, kCyan+2, kBlue+3,  kRed+3     };
@@ -240,11 +280,13 @@ void plot( TList *hists, TString histName, vector<double> &effs, int counter ) {
   hist = lastHist;
   TString efficLabel = "(Overall Efficiciency)";
   while ( hist ) {
-    hist->Scale(100.);
-    hist->GetYaxis()->SetRangeUser(0.,100.);
     hist->Draw();
-    double effic = 0;
-    if ( histName.Contains("TurnOn") ) {
+    double effic = -1;
+    if ( histType == "Efficiency" || histType == "TurnOn" ) {
+      hist->Scale(100.);
+      hist->GetYaxis()->SetRangeUser(0.,100.);
+    }
+    if ( histType == "TurnOn" ) {
       turnOn->SetParameters(1,20,100);
       turnOn->SetLineColor( hist->GetLineColor() );
       hist->Fit("turnOn");
@@ -252,9 +294,9 @@ void plot( TList *hists, TString histName, vector<double> &effs, int counter ) {
       efficLabel = "(Plateau Value)";
       hist->Draw();
     }
-    else effic = effs[hists->IndexOf(hist)];
+    if ( histType == "Efficiency" ) effic = effs[hists->IndexOf(hist)];
     TString title = hist->GetTitle();
-    if ( effic < 100. ) hist->SetTitle(title + Form(" (%.1f%)",effic));
+    if ( effic < 100. && effic > 0. ) hist->SetTitle( title + Form(" (%.1f%)",effic) );
     hist = (TH1F*)hists->Before(hist);
   }
 
@@ -269,10 +311,17 @@ void plot( TList *hists, TString histName, vector<double> &effs, int counter ) {
   double upperYBound = lowerYBound + (0.055 * hists->Capacity());
   TLegend* legend = gPad->BuildLegend(0.35,lowerYBound,0.83,upperYBound,
 				      "Legend:   Sample Name "+efficLabel);
+
+  if ( histType == "Efficiency" ) {
+    TString axisTitle = firstHist->GetXaxis()->GetTitle();
+    if ( axisTitle.Contains("#eta") ) histType = "#eta Efficiency";
+    if ( axisTitle.Contains("#phi") ) histType = "#phi Efficiency";	 
+  }
   TString number = Form("%.2i",counter);
-  if ( histName.Contains("TurnOn") ) 
-       lastHist->SetTitle(number+": "+label+cutEta);
-  else lastHist->SetTitle(number+": "+label+cutPtEta);
+  TString newTitle = number+": "+label+" "+histType;
+  if ( histType == "TurnOn" ) newTitle += cutEta;
+  else                        newTitle += cutPtEta;
+  lastHist->SetTitle(newTitle);
   gPad->Update();
   c1->SaveAs(number + fileType);
 
